2dc9e4b43749ddd442a72c60cf6c1d7f1140578b
yaseminvural99 <57195825+yaseminvural99@users.noreply.github.com>
2019-12-09 17:37:46 +0000

    Updated task class

--- src/test/java/tests/task.java
+++ src/test/java/tests/task.java
@@ -1,4 +1,8 @@
 package tests;
 
 public class task {
+  public static void main(String[]args){
+    System.out.println("Hello World");
+    //this line added from GitHub,see in intellij
+                       
 }
