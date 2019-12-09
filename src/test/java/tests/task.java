0409a54229ceb3fd160dbaef8419bde2068d9e70
yaseminvural99 <yasemin.vural99@gmail.com>
2019-12-09 18:15:40 +0000

    add void method

--- src/test/java/tests/task.java
+++ src/test/java/tests/task.java
@@ -1,8 +1,14 @@
 package tests;
 
 public class task {
-  public static void main(String[]args){
-    System.out.println("Hello World");
-    //this line added from GitHub,see in intellij
-                       
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        //this line added from GitHub,see in intellij
+
+    }
+
+    public void hi(){
+        System.out.println("Hi from intelliJ");
+    }
 }
+//git pull and click anywhere in intellij you will see
